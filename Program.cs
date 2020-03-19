e0e50c8f37135d694b90fb80a507de5d99563595
bagus-ganteng <61874925+bagus-ganteng@users.noreply.github.com>
2020-03-19 23:50:17 +0700

    File Object and Class
    Upload File

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object taxi
+            Taxi taxi = new Taxi();
+
+            //set nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            //panggil method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+       
+        }
+    }
+}
---
+++ Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Taxi
+    {
+        public string DriverName;
+        public bool OnDuty;
+        public int NumPassenger;
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}  ", this.DriverName );
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On Duty : Yes");
+            }
+            else
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number of Passenger : {0}  ", this.NumPassenger);
+        }
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Joni sedang menjemput penumpang");
+        }
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Joni sedang mengantar penumpang");
+        }
+    }
+
+}
